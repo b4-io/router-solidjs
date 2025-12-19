ac2f01e4cf97cef46a3597204d0f48669dec3a92
Bruno <brunodesimone17@gmail.com>
2025-12-19 16:20:24 -0300

    keep solidjs only docs

--- e2e/vue-router/sentry-integration/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { sentryVitePlugin } from '@sentry/vite-plugin'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vueJsx(), sentryVitePlugin()],
-})
